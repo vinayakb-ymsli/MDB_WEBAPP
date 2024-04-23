8ce933dd9f24aa5d2e015dff07fc1ec334b91321
AAryann <aryanj@yamha-motor-india.com>
2024-04-23 10:07:29 +0530

    background Image

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,27 +7,55 @@ export default function Body() {
         <>
             <Banner />
 
-            <div className="holder">
+            <div className="bkgimg">
+                <img src="/images/kv_pc.jpg" alt="" />
+            </div>
 
-                <div className="bkgimg">
-                    <img src="/images/kv_pc.jpg" alt="" />
+            <div className="intropage">
+                <div className="machineimg">
+                    <img src="/images/kv_main01.png" alt="" />
                 </div>
+
+                <div className="introtext">
+                    <h3>
+                        Cell picking & imaging System
+                    </h3>
+
+                    <h1>
+                        CELL HANDLER
+                    </h1>
+
+                    <h3>
+                        Advancing cell research into a new era
+                    </h3>
+
+                </div>
+
+                <div className="uploadbutton">
+                <button>
+                        Upload
+                </button>
+                </div>
+            </div>
+
+
+            {/* <div className="holder">
 
                 <div className="upload">
                     <h1>+</h1>
-                    {/* <div className="imageupload">
+                    <div className="imageupload">
                         upload
-                    </div> */}
+                    </div>
                 </div>
 
                 <div className="divider">
-
+                    
                 </div>
 
                 <div className="download">
                     <h1>+</h1>
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
