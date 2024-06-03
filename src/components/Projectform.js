7ec0314c11f716468b7761e6bd55c226db400de3
DevankGarg <devankg@yamaha-motor-india.com>
2024-06-03 16:56:26 +0530

    integrate forms for model project and client

--- src/components/Projectform.js
+++ src/components/Projectform.js
@@ -4,10 +4,16 @@ import { AiFillPlusCircle } from "react-icons/ai";
 import { RxCrossCircled } from "react-icons/rx";
 import "../styles/Projectform.css";
 import request from "superagent";
+import NotificationPopup from "./NotificationPopup";
 
 const CreateForm = ({ nameB, toggleForm, typeForm }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const closeNotification = () => {
+    setErrorMessage(null);
+  };
   let token = localStorage.getItem("token");
+
   const [projectFormData, setProjectFormData] = useState({
     projectName: "",
     folderName: "",
@@ -32,20 +38,6 @@ const CreateForm = ({ nameB, toggleForm, typeForm }) => {
     }
   }, [toggleForm]);
 
-  const handleProjectChange = (e) => {
-    setProjectFormData({
-      ...projectFormData,
-      [e.target.name]: e.target.value,
-    });
-    console.log("client form ", projectFormData);
-  };
-
-  const handleProjectSubmit = (e) => {
-    e.preventDefault();
-    console.log("Project Form submitted:", projectFormData);
-    setIsOpen(false);
-  };
-
   const handleClientChange = (e) => {
     setClientFormData({
       ...clientFormData,
@@ -57,23 +49,59 @@ const CreateForm = ({ nameB, toggleForm, typeForm }) => {
     e.preventDefault();
     console.log("Client Form submitted:", clientFormData);
     console.log(token);
-  
     setIsOpen(false);
-  
+
     try {
       const response = await request
         .post("https://ejmnmassds.ap-south-1.awsapprunner.com/create-client")
-        .send({"client_name":clientFormData.folderName})  // Ensure clientFormData is the payload
+        .send({ client_name: clientFormData.folderName.toUpperCase() }) 
         .set("Content-Type", "application/json")
         .set("Authorization", `${token}`);
-  
+
       console.log("Response:", response.body);
+      setErrorMessage("Created Client Successfully");
     } catch (error) {
-      console.error("Error:", error.response ? error.response.body : error.message);
+      console.error(
+        "Error:",
+        error.response ? error.response.body : error.message
+      );
     }
   };
 
-  const handleModelChange = (e) => {
+  const handleProjectChange = (e) => {
+    setProjectFormData({
+      ...projectFormData,
+      [e.target.name]: e.target.value,
+    });
+    console.log("client form ", projectFormData);
+  };
+
+  const handleProjectSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Project Form submitted:", projectFormData);
+    setIsOpen(false);
+
+    try {
+      const response = await request
+        .post("https://ejmnmassds.ap-south-1.awsapprunner.com/create-project")
+        .send({
+          client_name: "ravinder_tester32",
+          project_name: projectFormData.folderName.toUpperCase()
+        })
+        .set("Content-Type", "application/json")
+        .set("Authorization", `${token}`);
+
+      console.log("Response:", response.body);
+      setErrorMessage("Created Project Successfully");
+    } catch (error) {
+      console.error(
+        "Error:",
+        error.response ? error.response.body : error.message
+      );
+    }
+  };
+
+  const handleModelChange = async (e) => {
     const { name, value, files } = e.target;
     setModelFormData((prevState) => ({
       ...prevState,
@@ -82,10 +110,30 @@ const CreateForm = ({ nameB, toggleForm, typeForm }) => {
     console.log("client form ", modelFormData);
   };
 
-  const handleModelSubmit = (e) => {
+  const handleModelSubmit = async (e) => {
     e.preventDefault();
     console.log("Model Form submitted:", modelFormData);
     setIsOpen(false);
+
+    try {
+      const response = await request
+        .post("https://ejmnmassds.ap-south-1.awsapprunner.com/create-model")
+        .send({
+          client_name: "ravinder_tester32",
+          project_name: "REACT",
+          model_name: modelFormData.modelName.toUpperCase()
+        })
+        .set("Content-Type", "application/json")
+        .set("Authorization", `${token}`);
+
+      console.log("Response:", response.body);
+      setErrorMessage("Created Project Successfully");
+    } catch (error) {
+      console.error(
+        "Error:",
+        error.response ? error.response.body : error.message
+      );
+    }
   };
 
   const renderProjectForm = () => (
@@ -312,7 +360,15 @@ const CreateForm = ({ nameB, toggleForm, typeForm }) => {
       <button onClick={() => setIsOpen(true)} className="button-project-form">
         Create {nameB} <AiFillPlusCircle className="text-xl" />
       </button>
-
+      {errorMessage && (
+        <div className="error-message">
+          <NotificationPopup
+            message={errorMessage}
+            duration={3000}
+            onClose={closeNotification}
+          />
+        </div>
+      )}
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         {console.log(typeForm, nameB)}
         {typeForm === "project"
