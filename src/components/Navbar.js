083f898c2a0bb1632042e8f34fdd8b8be21492dd
Yash Magoo <ymagoo@yamaha-motor-india.com>
2024-06-10 18:08:41 +0530

    navbar href tag removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,26 +79,26 @@ const Navbar = ({ setisBlurr }) => {
             <div className="dropdown">
               <span className="dropbtn">Repository</span>
               <div className="dropdown-content">
-                <a className="inactive-link" href="#">Analytics</a>
-                <a className="inactive-link" href="#">Structure</a>
-                <a className="inactive-link" href="#">History</a>
+                <a className="inactive-link" >Analytics</a>
+                <a className="inactive-link" >Structure</a>
+                <a className="inactive-link" >History</a>
                 <a className="active-link" href="/projects">View Clients</a>
                 <a className="active-link" href="/projects/addclient">Add Client</a>
 
                 <a className="active-link" href="/projects/addproject">Add Project</a>
 
                 <a className="active-link" href="/projects/addmodel">Add Model</a>
-                <a className="inactive-link" href="#">Search a Repository</a>
+                <a className="inactive-link" >Search a Repository</a>
               </div>
             </div>
 
             <div className="dropdown">
               <span className="dropbtn">Visualization</span>
               <div className="dropdown-content">
-                <a className="inactive-link" href="#">Input Images</a>
-                <a className="inactive-link" href="#">Output Images</a>
+                <a className="inactive-link" >Input Images</a>
+                <a className="inactive-link" >Output Images</a>
                 <a className="active-link"
-                  href="#"
+                  
                   onClick={() =>
                     openPopup(
                       "A false positive in a machine learning model occurs when it mistakenly identifies a negative outcome as positive, leading to an incorrect prediction. It signifies a type of error where the model wrongly indicates the presence of a condition or event that is actually absent.",
@@ -110,7 +110,7 @@ const Navbar = ({ setisBlurr }) => {
                   False Positive comparison
                 </a>
                 <a className="active-link"
-                  href="#"
+                  
                   onClick={() =>
                     openPopup(
                       "Cell Segmentation is a task of splitting a microscopic image domain into segments, which represent individual instances of cells. It is a fundamental step in many biomedical studies, and it is regarded as a cornerstone of image-based cellular research. Cellular morphology is an indicator of a physiological state of the cell, and a well-segmented image can capture biologically relevant morphological information.",
@@ -121,9 +121,9 @@ const Navbar = ({ setisBlurr }) => {
                 >
                   Number of cells segmented
                 </a>
-                <a className="inactive-link" href="#">Inference time graph</a>
+                <a className="inactive-link" >Inference time graph</a>
                 <a className="active-link"
-                  href="#"
+                  
                   onClick={() =>
                     openPopup(
                       "The region of interest (often abbreviated ROI) is a sample within a data set identified for a particular purpose. The concept of a ROI is commonly used in many application areas. For example, in medical imaging, the boundaries of a tumor may be defined on an image or in a volume, for the purpose of measuring its size",
@@ -140,11 +140,11 @@ const Navbar = ({ setisBlurr }) => {
             <div className="dropdown">
               <span className="dropbtn">Model</span>
               <div className="dropdown-content">
-                <a className="inactive-link" href="#">Algorithms</a>
-                <a className="inactive-link" href="#">Intution</a>
-                <a className="inactive-link" href="#">Improvement area</a>
-                <a className="inactive-link" href="#">Comparison with older models</a>
-                <a className="inactive-link" href="#">Versioning</a>
+                <a className="inactive-link" >Algorithms</a>
+                <a className="inactive-link" >Intution</a>
+                <a className="inactive-link" >Improvement area</a>
+                <a className="inactive-link" >Comparison with older models</a>
+                <a className="inactive-link" >Versioning</a>
               </div>
             </div>
 
@@ -152,10 +152,9 @@ const Navbar = ({ setisBlurr }) => {
               <span className="dropbtn">Metrics</span>
               <div className="dropdown-content">
                 
-                <a className="inactive-link" href="#">Precision recall</a>
-                <a className="inactive-link" href="#">F1 score</a>
+                <a className="inactive-link" >Precision recall</a>
+                <a className="inactive-link" >F1 score</a>
                 <a className="active-link"
-                  href="#"
                   onClick={() =>
                     openPopup(
                       "The F1 score is a metric used in machine learning to evaluate the performance of classification models. It’s particularly useful when the classes are imbalanced. The F1 score is the harmonic mean of precision and recall, which are two critical measures in classification tasks:",
@@ -166,14 +165,14 @@ const Navbar = ({ setisBlurr }) => {
                 >
                   Accuracy
                 </a>
-                <a className="inactive-link" href="#">Intersection over union</a>
-                {/* <a className="inactive-link" href="#">Area based normality</a> */}
-                {/* <a className="inactive-link" href="#">Weights and parameters</a> */}
-                <a className="inactive-link" href="#">Precision curve</a>
-                {/* <a className="inactive-link" href="#">Pixel based confusion matrix</a>
-                <a className="inactive-link" href="#">Pixel based sensitivity curve</a>
-                <a className="inactive-link" href="#">Pixel based specificity curve</a> */}
-                <a className="inactive-link" href="#">Picking condition</a>
+                <a className="inactive-link" >Intersection over union</a>
+                {/* <a className="inactive-link" >Area based normality</a> */}
+                {/* <a className="inactive-link" >Weights and parameters</a> */}
+                <a className="inactive-link" >Precision curve</a>
+                {/* <a className="inactive-link" >Pixel based confusion matrix</a>
+                <a className="inactive-link" >Pixel based sensitivity curve</a>
+                <a className="inactive-link" >Pixel based specificity curve</a> */}
+                <a className="inactive-link" >Picking condition</a>
               </div>
             </div>
 
